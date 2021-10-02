0019e96fe0f4a51d01b652a611429ecfb6622930
Anni Priscilla A <63101227+annipriscillaa@users.noreply.github.com>
2021-10-02 20:55:02 +0530

    Added core team member

--- procode-frontend/src/views/Core/Core.js
+++ procode-frontend/src/views/Core/Core.js
@@ -6,6 +6,7 @@ import ajay_img from "../../img/ajay2.jpg";
 import obed_img from "../../img/obed2.jpg";
 import michael_img from "../../img/michael2.jpg";
 import maddy_img from "../../img/maddy2.jpg";
+import jothika_img from "../../img/jothika2.jpeg";
 export const Core = () => {
   return (
     <div>
@@ -181,6 +182,35 @@ export const Core = () => {
               <div className="col-md-6 col-sm-12 d-flex align-items-center">
                 <div className="info">
                   <p>Madhavan V</p>
+                  <p>Core Team Member</p>
+                  <p>Final Year IT</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="before_box">
+          <div class=" box_for_core_members">
+            <div className="row mb-5">
+              <div className="col-md-6 col-sm-12 d-flex justify-content-center">
+                <div
+                  className="image"
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    margin: "10%",
+                  }}
+                >
+                  <img
+                    src={jothika_img}
+                    style={{ width: "100%", height: "98%" }}
+                    alt="Jothika Image"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6 col-sm-12 d-flex align-items-center">
+                <div className="info">
+                  <p>Jothika R</p>
                   <p>Core Team Member</p>
                   <p>Final Year IT</p>
                 </div>
